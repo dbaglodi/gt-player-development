a0d8ec61ac663a955adeec21170169a2daefdff0
dbaglodi <baglodidisha@gmail.com>
2023-11-03 18:41:21 -0400

    added some data on the names of players

--- transfer-data.py
+++ transfer-data.py
@@ -1,1 +1,5 @@
 import numpy as np
+import pandas as pd
+
+names = np.array(['James Banks', 'Bubba Parham', 'Jordan Usher', 'Kyle Sturdivant', 'Rodney Howard', 'Deivon Smith', 'Ja\'von Franklin', 'Lance Terry'])
+df = pd.DataFrame(names)
