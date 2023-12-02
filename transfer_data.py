15a0fc4930adf7cf96fa34d825d19261a6d1242c
dbaglodi <baglodidisha@gmail.com>
2023-12-02 17:55:27 -0500

    more data
    added distinctions between transfers and non-transfers

--- transfer_data.py
+++ transfer_data.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-names = np.array(['James Banks', 'Bubba Parham', 'Jordan Usher', 'Kyle Sturdivant', 'Rodney Howard', 'Deivon Smith', 'Ja\'von Franklin', 'Lance Terry'])
+names = np.array(['James Banks III F', 'Bubba Parham G', 'Jordan Usher G', 'Kyle Sturdivant G', 'Rodney Howard F', 'Deivon Smith G', 'Ja\'von Franklin F', 'Lance Terry G'])
 years = np.array(['2018', '2019', '2019', '2020', '2020', '2021', '2022', '2022'])
 transferred_from = np.array(['University of Texas', 'Virginia Military Institute', 'University of South Carolina', 'University of South Carolina', 'University of Georgia', 'Mississippi State University', 'University of South Alabama', 'Gardner-Webb University'])
 games_played_at_prev_school = np.array([46, 62, 48, 21, 24, 33, 28, 54])
@@ -17,5 +17,8 @@ players = pd.DataFrame({
     'Total Games Played': games_played_at_prev_school + games_played_at_gt,
     'Total Points Scored at Previous School': points_scored_at_prev_school,
     'Total Points Scored at GT': points_scored_at_gt,
-    'Total Points Scored': points_scored_at_prev_school + points_scored_at_gt
+    'Total Points Scored': points_scored_at_prev_school + points_scored_at_gt,
+    'Average Points per Game at Previous School': points_scored_at_prev_school / games_played_at_prev_school,
+    'Average Points per Game at GT': points_scored_at_gt / games_played_at_gt,
+    'Average Points per Game': (points_scored_at_prev_school + points_scored_at_gt) / (games_played_at_prev_school + games_played_at_gt)
 })
